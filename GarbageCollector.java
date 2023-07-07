9798780b38ea0b6efd851fa4ff1dfe8b89af82c7
Jason-Tysl <jktysl@gmail.com>
2023-07-07 00:08:06 -0400

    initial commit with first gc functions

---
+++ GarbageCollector.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class GarbageCollector {
+
+    static List<Integer> theGarbageCollector;
+
+    // initialize garbage collector
+    public static void initializeGC() {
+
+    }
+
+    // make space for new garbage to be collected
+    public static void allocateGC() {
+
+    }
+
+    // add a record variable to gc list
+    public static void addVarToGC(String id) {
+
+    }
+
+    // take away a record variable from the gc list
+    public static void takeVarFromGC(String id) {
+        
+    }
+}
